91777a5d8569279d21fb9420522d0672a748c1a6
johannkrugell <johannkrugell@gmail.com>
2023-10-24 06:50:00 +1100

    Add Airport model and seed data Add the airport model and seed data.  The airport model has the following fields: iata_code: This will store the unique 3-letter code used internationally to identify airports. location: This might represent the city or locality of the airport. name: This is the full name of the airport. country: The country where the airport is located.
    The seed data is a list of the internal flights in Australia busiest
    airports in the world.
    
    On branch airport-model
    Changes to be committed:
    	new file:   app/models/airport.rb
    	new file:   db/migrate/20231023193444_create_airports.rb
    	new file:   db/schema.rb
    	modified:   db/seeds.rb
    	new file:   test/fixtures/airports.yml
    	new file:   test/models/airport_test.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+# Seed the airports table
+# db/seeds.rb
+
+australian_airports = [
+  { location: "Adelaide", name: "Adelaide Airport", iata_code: "ADL" },
+  { location: "Brisbane", name: "Brisbane Airport", iata_code: "BNE" },
+  { location: "Broome", name: "Broome International Airport", iata_code: "BME" },
+  { location: "Cairns", name: "Cairns Airport", iata_code: "CNS" },
+  { location: "Canberra", name: "Canberra Airport", iata_code: "CBR" },
+  { location: "Darwin", name: "Darwin International Airport", iata_code: "DRW" },
+  { location: "Geelong", name: "Avalon Airport", iata_code: "AVV" },
+  { location: "Gold Coast", name: "Gold Coast Airport", iata_code: "OOL" },
+  { location: "Hobart", name: "Hobart Airport", iata_code: "HBA" },
+  { location: "Melbourne", name: "Melbourne Airport", iata_code: "MEL" },
+  { location: "Newcastle", name: "Newcastle Airport", iata_code: "NTL" },
+  { location: "Perth", name: "Perth Airport", iata_code: "PER" },
+  { location: "Port Hedland", name: "Port Hedland International Airport", iata_code: "PHE" },
+  { location: "Sunshine Coast", name: "Sunshine Coast Airport", iata_code: "MCY" },
+  { location: "Sydney", name: "Sydney Airport", iata_code: "SYD" },
+  { location: "Townsville", name: "Townsville Airport", iata_code: "TSV" }
+]
+
+australian_airports.each do |airport|
+  Airport.create!(
+    location: airport[:location],
+    name: airport[:name],
+    iata_code: airport[:iata_code],
+    country: "Australia"
+  )
+end
